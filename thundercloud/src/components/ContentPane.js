472637f5b4440037bcdeb142ed9f8380c2f512f0
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-17 10:21:05 -0700

    Added title to content pane

--- thundercloud/src/components/ContentPane.js
+++ thundercloud/src/components/ContentPane.js
@@ -1,9 +1,16 @@
 import React from "react";
 import MainPlot from "./MainPlot";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 export default function ContentPane(props) {
 
   return (
-    <MainPlot/>
+    <Box>
+      <Box mb={1}>
+        <Typography variant="h4">Garden Status</Typography>
+      </Box>
+      <MainPlot/>
+    </Box>
   )
 }
